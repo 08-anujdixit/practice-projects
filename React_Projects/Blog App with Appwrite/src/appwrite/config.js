c97a932a27a484b64f6421e3aa6a210422235b74
08-anujdixit <anuj111dixit@gmail.com>
2026-06-13 12:27:43 +0530

    Appwrite services config file done!

--- React_Projects/Blog App with Appwrite/src/appwrite/config.js
+++ React_Projects/Blog App with Appwrite/src/appwrite/config.js
@@ -16,4 +16,114 @@ export class Service{
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);    
     }
+
+    async createPost({title, content, slug, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                ID.unique(),
+                {
+                    title,
+                    slug,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                },
+            );
+        } catch (error) {
+            console.log("APPWRITE : CREATE POST ERROR : CONFIG SERVICE ERROR ", error);
+        }
+    }
+
+    async updatePost(postId ,{title,content,slug, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                postId,
+                {
+                    title,
+                    slug,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log('APPWRITE : UPDATE DOCUMENT ERROR : APPWRITE : CONFIG : ', error);
+        }
+    }
+
+    async deletePost(postId){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                postId,
+            );
+            return true;
+        } catch (error) {
+            console.log("APPWRITE : DELETE POST ERROR : APPWRITE CONFIG SERVICE ERROR : ",error);
+            return false;
+        }
+    }
+
+    async getPost(postId){
+        try {
+                return await this.databases.getDocument(
+                    conf.appwriteDatabaseId,
+                    conf.appwriteCollectionId,
+                    postId,
+                ); 
+        } catch (error) {
+            console.log('APPWRITE : GET POST ERROR : CONFIG FILE SERVICE :', error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal('status', 'active')]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            );
+        } catch (error) {
+            console.log("APPWRITE : GET POSTS ERROR : APPWRITE CONFIG SERVICE ERROR : ", error);
+            return false;
+        }
+    }
+
+
+    // File methods : 
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            );
+        } catch (error) {
+            console.log("APPWRITE: UPLOAD FILE ERROR: CONFIG SERVICE ERROR ",error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+            return true;
+        } catch (error) {
+            console.log("APPWRITE: DELETE FILE ERROR : CONFIG SERVICE ERROR : ", error);
+            return false;
+        }
+    }
+
+    getFileView(fileID){
+        return this.bucket.getFileView(conf.appwriteBucketId, fileId);
+    }
+
 }
