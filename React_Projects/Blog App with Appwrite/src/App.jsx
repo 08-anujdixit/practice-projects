bc53070b83573e67213df37fd2bdeaa88db02588
08-anujdixit <anuj111dixit@gmail.com>
2026-06-14 00:01:41 +0530

    auto login setup done

--- React_Projects/Blog App with Appwrite/src/App.jsx
+++ React_Projects/Blog App with Appwrite/src/App.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import {Header, Footer} from './components/index'
-
-
+import useDispatch from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice.js'
 
 export default function App() {
+
+  const[loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser().then((userData) =>{
+      if(userData){
+        dispatch(login(userData));
+      }else{
+        dispatch(logout());
+      }
+    }).catch(error=> console.log("Session expired please login again", error) )
+    .finally(()=>setLoading(false));
+  }, []);
+
   return (
     <>
       <Header/>
