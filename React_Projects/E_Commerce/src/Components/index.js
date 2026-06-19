9ec4c7488b850f2e0be6a16b6a82bab4a9878abb
08-anujdixit <anuj111dixit@gmail.com>
2026-06-19 22:48:20 +0530

    index file added for component import export

---
+++ React_Projects/E_Commerce/src/Components/index.js
@@ -0,0 +1,9 @@
+import Header from './Header/Header.jsx'
+import Hero from './Hero.jsx'
+import Categories from './Categories/Categories.jsx'
+
+export default {
+    Header,
+    Hero,
+    Categories,
+}
