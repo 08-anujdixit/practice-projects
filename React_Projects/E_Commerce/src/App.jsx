6d5fa740845bcf2ab6a073bf6cb01fac01fcc79e
08-anujdixit <anuj111dixit@gmail.com>
2026-06-30 17:11:05 +0530

    Scroll to top functionality added

--- React_Projects/E_Commerce/src/App.jsx
+++ React_Projects/E_Commerce/src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Header, Footer } from "./Components/index";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header />
 
       <main className="flex-1">
