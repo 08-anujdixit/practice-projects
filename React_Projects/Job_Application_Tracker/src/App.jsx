e084dc14549fef4bc8b3cbac07623e218c2a3dbc
08-anujdixit <anuj111dixit@gmail.com>
2026-09-21 11:47:16 +0530

    Initialize Job Application Tracker project with React, Vite, and Tailwind CSS
    - Created package.json with dependencies and scripts for development
    - Added main application component (App.jsx) with basic structure
    - Included Tailwind CSS styles in index.css
    - Set up entry point (main.jsx) for rendering the application
    - Configured Vite with React and Tailwind CSS plugins

---
+++ React_Projects/Job_Application_Tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <><div className='text-green-600'>Hello WOrld </div></>
+  )
+}
+
+export default App
